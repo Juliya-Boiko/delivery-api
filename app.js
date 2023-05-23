59198a6330e2574e72cd79fd58bdfbe2ea6af082
Juliya Boiko <boikojuliya2707@gmail.com>
2023-05-23 21:12:52 +0300

    add dependencies

---
+++ app.js
@@ -0,0 +1,33 @@
+// const express = require('express');
+// const logger = require('morgan');
+// const cors = require('cors');
+// require('dotenv').config();
+
+// const productsRouter = require('./routes/products');
+// const reviewsRouter = require('./routes/reviews');
+// const newsRouter = require('./routes/news');
+// const authRouter = require('./routes/auth');
+
+// const app = express();
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+// app.use(logger(formatsLogger));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.static("public"));
+
+// app.use('/products', productsRouter);
+// app.use('/reviews', reviewsRouter);
+// app.use('/news', newsRouter);
+// app.use('/auth', authRouter);
+
+// app.use((req, res) => {
+//   res.status(404).json({ message: 'Not found' });
+// });
+
+// app.use((err, req, res, next) => {
+//   const { status = 500, message = 'Server ERROR' } = err;
+//   res.status(status).json({ message });
+// });
+
+// module.exports = app;
