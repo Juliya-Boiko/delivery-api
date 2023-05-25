5ccaa43d05758bc8f38cb86e8ede514479d6438a
Juliya Boiko <boikojuliya2707@gmail.com>
2023-05-25 17:26:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ require('dotenv').config();
 // UTILS & LOGGERS
 const app = express();
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,  Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", true)
+  next();
 });
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
