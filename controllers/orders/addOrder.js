7d8dc0043df9ede39313bcccf6af7a2f502a9623
Juliya Boiko <boikojuliya2707@gmail.com>
2023-05-25 16:48:17 +0300

    add res:headers

--- controllers/orders/addOrder.js
+++ controllers/orders/addOrder.js
@@ -1,12 +1,14 @@
-// const Order = require('../../models/order');
+const Order = require('../../models/order');
 
 const addOrder = async (req, res) => {
-  // const data = await Order.create({ ...req.body });
+  const data = await Order.create({ ...req.body });
 
-  res.status(200).json({ 
-    status: 'Order added',
-    data: req.body
-   });
+  res
+    .header('Access-Control-Allow-Origin', '*')
+    .header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    .header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept, Origin, X-Requested-With')
+    .status(200)
+    .json({ data });
 };
 
 module.exports = addOrder;
