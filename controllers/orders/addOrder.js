569b86782a33e7ca0cb1c5ab12508c8037305515
Juliya Boiko <boikojuliya2707@gmail.com>
2023-05-25 14:41:24 +0300

    add headers to response

--- controllers/orders/addOrder.js
+++ controllers/orders/addOrder.js
@@ -3,12 +3,14 @@ const Order = require('../../models/order');
 const addOrder = async (req, res) => {
   const data = await Order.create({ ...req.body });
 
-  res.header(
+  res
+    .header('Access-Control-Allow-Origin', '*')
+    .header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
-  ).status(200).json({
-    data
-  });
+    )
+    .status(200)
+    .json({ data });
 };
 
 module.exports = addOrder;
