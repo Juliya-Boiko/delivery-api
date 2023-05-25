143d608bda0770cae55cea601809b6d8ad703aba
Juliya Boiko <boikojuliya2707@gmail.com>
2023-05-25 16:34:52 +0300

    Update addOrder.js

--- controllers/orders/addOrder.js
+++ controllers/orders/addOrder.js
@@ -4,7 +4,8 @@ const addOrder = async (req, res) => {
   // const data = await Order.create({ ...req.body });
 
   res.status(200).json({ 
-    data: req.body
+    status: 'Order added',
+    // data
    });
 };
 
