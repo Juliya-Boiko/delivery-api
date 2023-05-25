2c0f4837223d3ea14f1b37e8ffb1eab08d653390
Juliya Boiko <boikojuliya2707@gmail.com>
2023-05-25 20:28:39 +0300

    update mongoose

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// })
+app.use((req, res ,next)=>{
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+  next();
+})
 
 // UTILS & LOGGERS
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
