ecc8ff31a05162a4db46182444d1269140a56a5e
Juliya Boiko <boikojuliya2707@gmail.com>
2023-05-25 14:33:34 +0300

    add res.headers

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+
+  next();
+});
 
 // ROUTES
 app.use('/delivery', shopsRouter);
--- controllers/orders/addOrder.js
+++ controllers/orders/addOrder.js
@@ -3,7 +3,10 @@ const Order = require('../../models/order');
 const addOrder = async (req, res) => {
   const data = await Order.create({ ...req.body });
 
-  res.status(200).json({
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  ).status(200).json({
     data
   });
 };
